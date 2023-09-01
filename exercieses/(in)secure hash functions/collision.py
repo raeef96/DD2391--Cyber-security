429c6ce7a402d9319daa98184d05e16f5d78c6b6
Rafael Bechara <75490904+raeef96@users.noreply.github.com>
2023-09-01 15:42:09 +0200

    add assignment

---
+++ exercieses/(in)secure hash functions/collision.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from insecure_hash import hash_string
+from Crypto.Cipher import AES
+
+
+def find_collision(message):
+    # Your solution.
+    return "".encode()
+
+
+if __name__ == '__main__':
+    message = "aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbb".encode()
+    print("Hash of %s is %s" % (message, hash_string(message)))
+    collision = find_collision(message)
+    print("Hash of %s is %s" % (collision, hash_string(collision)))
